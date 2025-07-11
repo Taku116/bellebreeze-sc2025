a02f597388f4e28586f942a314a1c3425ea5d301
Taku116 <okushima116@gmail.com>
2025-07-11 23:37:53 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 document.getElementById('menu-toggle').addEventListener('click', function () {
   document.getElementById('nav').classList.toggle('active');
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const overlay = document.getElementById('intro-overlay');
+  overlay.addEventListener('click', function () {
+    overlay.classList.add('fade-out');
+    setTimeout(() => {
+      overlay.style.display = 'none';
+    }, 1000);
+  });
+});
